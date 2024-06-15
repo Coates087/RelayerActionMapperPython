3bca3b7d68c4097027a7d28182362cf3d0892cda
Coates087 <coates000@gmail.com>
2024-06-15 18:17:40 -0500

    Update _compile-program-win.bat

--- _compile-program-win.bat
+++ _compile-program-win.bat
@@ -1,5 +1,6 @@
 @REM only run this script if PythonInstaller is installed (https://www.python.org/downloads/)
-@REM Note: type in "start _compile-program-win.bat" without quotes. Make sure to whitelist this directory with your anti-virus software
+@REM Note: type in "start _compile-program-win.bat" without quotes or type "_compile-program-win.bat" in the cmd terminal. 
+@REM ...Make sure to whitelist this directory with your anti-virus software
 
 xcopy /S /Q /Y /F "./resource_objects/RelayerIcon.ico" "./build/RelayerIcon.ico"*
 
